d443297709578ae2094c977ff715d798d2cae6f5
22051145-dmh <1092733131@qq.com>
2023-03-29 14:15:42 +0800

    danzhouqi NPC

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -11,29 +11,26 @@ static VerilatedVcdC * tfp;
 static vluint64_t main_time = 0;
 static const vluint64_t sim_time = 1000;
 
-// inst.bin
-// inst 0: 1 + zero = reg1 1+0=1
-// inst 1: 2 + zero = reg1 2+0=2
-// inst 2: 1 + reg1 = reg1 1+2=3
+
 int inst_rom[65536];
 
+void read_inst(char *file) {
+  FILE *fp = fopen(file, "rb");
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  size = fread(inst_rom, size, 1, fp);
+}
 
 uint32_t paddr_read(uint64_t addr, int len){
   int index = (addr - 0x80000000)/4;
-  if(index >= 0 && index <= 5)
-    return inst_rom[index];
+  return inst_rom[index];
   return 0;
 }
 
 int main(int argc, char **argv)
 {
-  inst_rom[0] = 0x00108093;
-  inst_rom[1] = 0x00108093;
-  inst_rom[2] = 0x00108093;
-  inst_rom[3] = 0x00108093;
-  inst_rom[4] = 0x00108093;
-  inst_rom[5] = 0x00108093;
-  // initialization
+  read_inst(argv[1]);
   Verilated::commandArgs(argc, argv);
   Verilated::traceEverOn(true);
 
@@ -43,7 +40,7 @@ int main(int argc, char **argv)
   top->trace(tfp, 5);
   tfp->open("top.vcd");
 	int i = 0;
-	while( !Verilated::gotFinish() && main_time < sim_time && i < 6)
+	while( !Verilated::gotFinish() && main_time < sim_time)
 	{
 	  if( main_time % 10 == 0 ) top->clk = 0;
 	  if( main_time % 10 == 5 ) top->clk = 1;
@@ -57,13 +54,14 @@ int main(int argc, char **argv)
 	    top->rst = 0;
 		if( main_time % 10 == 0 ) {
 		  top->inst = paddr_read(top->in_addr, 4);
+      printf("%08x\n", top->inst);
       i++;
-      printf("%08x\n",top->inst);
       }
 	  }
 	  top->eval();
 	  tfp->dump(main_time);
 	  main_time++;
+    if((i >5 && top->inst == 0)) break;
 	}
 		
   // clean
