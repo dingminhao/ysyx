4a501913fe38bd6227d3e711e1ee07f254734d77
22051145-dmh <1092733131@qq.com>
2023-04-26 14:17:13 +0800

    dl_fantui

--- npc/vsrc/core/fetch.v
+++ npc/vsrc/core/fetch.v
@@ -37,27 +37,27 @@ module fetch (
 
   assign inst_data_o = _mem_data[31:0];
 
-  /***********************TRAP**********************/
-  wire _Instruction_address_misaligned = `FALSE;
-  wire _Instruction_access_fault = `FALSE;
-  wire _Instruction_page_fault = `FALSE;
+  // /***********************TRAP**********************/
+  // wire _Instruction_address_misaligned = `FALSE;
+  // wire _Instruction_access_fault = `FALSE;
+  // wire _Instruction_page_fault = `FALSE;
 
-  reg [`TRAP_BUS] _if_trap_bus;
-  integer i;
-  always @(*) begin
-    for (i = 0; i < `TRAP_LEN; i = i + 1) begin
-      if (i == `TRAP_INST_ADDR_MISALIGNED) begin
-        _if_trap_bus[i] = _Instruction_address_misaligned;
-      end else if (i == `TRAP_INST_ACCESS_FAULT) begin
-        _if_trap_bus[i] = _Instruction_access_fault;
-      end else if (i == `TRAP_INST_PAGE_FAULT) begin
-        _if_trap_bus[i] = _Instruction_page_fault;
-      end else begin
-        _if_trap_bus[i] = `FALSE;
-      end
-    end
-  end
-  assign trap_bus_o = _if_trap_bus;
+  // reg [`TRAP_BUS] _if_trap_bus;
+  // integer i;
+  // always @(*) begin
+  //   for (i = 0; i < `TRAP_LEN; i = i + 1) begin
+  //     if (i == `TRAP_INST_ADDR_MISALIGNED) begin
+  //       _if_trap_bus[i] = _Instruction_address_misaligned;
+  //     end else if (i == `TRAP_INST_ACCESS_FAULT) begin
+  //       _if_trap_bus[i] = _Instruction_access_fault;
+  //     end else if (i == `TRAP_INST_PAGE_FAULT) begin
+  //       _if_trap_bus[i] = _Instruction_page_fault;
+  //     end else begin
+  //       _if_trap_bus[i] = `FALSE;
+  //     end
+  //   end
+  // end
+  // assign trap_bus_o = _if_trap_bus;
 
 
 endmodule
