1f8adcb27c502134214844b9c1a56d91fe4b02cb
22051145-dmh <1092733131@qq.com>
2023-05-11 10:02:41 +0800

    dl_fantui

--- npc/vsrc/core/icache_data.v
+++ npc/vsrc/core/icache_data.v
@@ -6,25 +6,25 @@ module icache_data #(
     BLK_LEN = 4,  // 块内地址 长度
     TAG_NUM = 32  // tag 个数
 ) (
-    input clk,
-    input rst,
+    input clk, // 时钟
+    input rst, // 复位
     input [IDX_LEN-1:0] icache_index_i,  // index
-    input [BLK_LEN-1:0] icache_blk_addr_i,
-    input [128-1:0] icache_line_wdata_i,
-    input  [128-1:0] icache_wmask,
-    input icache_wen_i,
-    output [128-1:0]    icache_line_rdata_o
+    input [BLK_LEN-1:0] icache_blk_addr_i, // 块内地址
+    input [128-1:0] icache_line_wdata_i, // 块写入数据
+    input  [128-1:0] icache_wmask, // 写入掩码
+    input icache_wen_i, // 写使能
+    output [128-1:0]    icache_line_rdata_o // 块读出数据
 );
-
-
+// 为什么这里需要利用128位的数据来进行读写呢？
+// 因为这里的icache_line_rdata_o是一个128位的数据，而icache_line_wdata_i也是一个128位的数据
   S011HD1P_X32Y2D128_BW u_S011HD1P_X32Y2D128_BW (
-      .Q   (icache_line_rdata_o),
-      .CLK (clk),
+      .Q   (icache_line_rdata_o), // 读出数据
+      .CLK (clk), // 时钟
       .CEN (1'b0), // 低电平有效
-      .WEN (~icache_wen_i),
-      .BWEN(~icache_wmask),
-      .A   ({1'b0,icache_index_i}),
-      .D   (icache_line_wdata_i)
+      .WEN (~icache_wen_i), // 低电平有效
+      .BWEN(~icache_wmask), // 低电平有效
+      .A   ({1'b0,icache_index_i}), // 地址
+      .D   (icache_line_wdata_i) // 写入数据
   );
 
 
